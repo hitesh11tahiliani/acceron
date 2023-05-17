357c8c3f452a1c4ad94475ade7be9e0bc78d2f5a
Hitesh <hitesh11tahiliani@gmail.com>
2023-05-17 22:38:20 +0530

    darpan

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar"
 import Home from "./Components/Home"
 import Quotation from './Components/Quotation';
